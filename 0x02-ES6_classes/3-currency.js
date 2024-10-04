f786b309e66e31b2899157ef3adb0d0929472d86
Obi Chukwu <kennetho.onu@gmail.com>
2024-10-04 03:46:29 +0100

    currency class

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-underscore-dangle */
+export default class Currency {
+  constructor(code, name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    } else {
+      this._name = name;
+      this._code = code;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = code;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
+/* eslint-disable no-underscore-dangle */

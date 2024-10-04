532967d645b157d07a4e594f61e6e3dd56b9b0b2
Obi Chukwu <kennetho.onu@gmail.com>
2024-10-04 03:43:09 +0100

    100-car class

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/extensions
+import Car from './10-car.js';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    const Species = super.constructor[Symbol.species];
+    return new Species();
+  }
+}

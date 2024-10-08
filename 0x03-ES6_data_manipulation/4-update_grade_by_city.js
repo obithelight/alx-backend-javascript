616841bc74d00171c5676b076bfe14338726a291
Obichukwu <kennetho.onu@gmail.com>
2024-10-08 08:00:05 +0000

    combined filter and map methods

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,9 @@
+export default function updateStudentGradeByCity(listStudents, city, newGrades) {
+  return listStudents
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => student.id === grade.studentId);
+      const grade = gradeObj ? gradeObj.grade : 'N/A';
+      return { ...student, grade };
+    });
+}

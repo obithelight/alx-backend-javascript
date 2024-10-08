27faba597f1ee10b49c6e4edf41d5bd99d402ae0
Obichukwu <kennetho.onu@gmail.com>
2024-10-08 04:40:10 +0000

    Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,7 @@
-/* eslint-disable no-underscore-dangle */
-
 export default function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 3, firstName: 'Serena', location: 'San Francisco' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
-
-/* eslint-disable no-underscore-dangle */

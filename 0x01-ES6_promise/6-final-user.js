58c198051caac47b9342c1736788f0fff0c49916
Obi Chukwu <kennetho.onu@gmail.com>
2024-10-14 03:57:30 +0100

    final user cleanup

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,9 +5,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const createPromise = signUpUser(firstName, lastName);
   const uploadPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([createPromise, uploadPromise])
-    .then((results) => results.map((result) => ({
+  return Promise.allSettled([createPromise, uploadPromise]).then((results) => {
+    return results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.error,
-    })));
+    }));
+  });
 }

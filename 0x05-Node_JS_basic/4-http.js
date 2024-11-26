017b806e0febacb11666fe541405c0d2c240b848
Obi Chukwu <kennetho.onu@gmail.com>
2024-11-26 23:15:20 +0100

    create a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,12 +5,12 @@
 const { createServer } = require('node:http');
 
 const app = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type', 'text/plain' });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 });
 
 app.listen(1245, () => {
-  console.log(`Server is listening at 1245`);
+  console.log('Server is listening at 1245');
 });
 
 module.export = app;

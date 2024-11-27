b3de6ab440cd31d1448b08668e8d982c4cb159ef
Obi Chukwu <kennetho.onu@gmail.com>
2024-11-27 05:46:26 +0100

    nodejs sever

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,15 +2,17 @@
  * create a mini http-server with nodejs
  */
 
-const { createServer } = require('node:http');
+const { createServer } = require('http');
+
+const port = 1245;
 
 const app = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 });
 
-app.listen(1245, () => {
-  console.log('Server is listening at 1245');
+app.listen(`${port}`, () => {
+  console.log(`Server is listening at port ${port}`);
 });
 
 module.export = app;

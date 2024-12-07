cc8ec51b29f442d113a33f6ee1790e967b0899c6
Obi Chukwu <kennetho.onu@gmail.com>
2024-12-07 15:12:32 +0000

    set up dev environment

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} arr - The source array.
+ * @author Obi Chukwu <https://github.com/obithelight>
+ * @returns {Set}
+ */
+export default function setFromArray(arr) {
+  return new Set(arr);
+}

c588c284920c4a706a972b40dd76a245c529e5a8
Obi Chukwu <kennetho.onu@gmail.com>
2024-12-09 15:24:01 +0100

    create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -68,7 +68,7 @@ const SERVER_ROUTE_HANDLERS = [
   {
     route: '/',
     handler(_, res) {
-      const responseText = 'Hello ALX!';
+      const responseText = 'Hello Holberton School!';
 
       res.setHeader('Content-Type', 'text/plain');
       res.setHeader('Content-Length', responseText.length);
